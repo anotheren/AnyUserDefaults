1de0f050324d37a04e6a58ebddb813309b2b5d99
liudong <liudong.edward@gmail.com>
2019-09-16 09:55:22 +0800

    Add Carthage Support

---
+++ Sources/AnyUserDefaults/AnyUserDefaults.h
@@ -0,0 +1,19 @@
+//
+//  AnyUserDefaults.h
+//  AnyUserDefaults
+//
+//  Created by 刘栋 on 2019/9/16.
+//  Copyright © 2019 anotheren.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnyUserDefaults.
+FOUNDATION_EXPORT double AnyUserDefaultsVersionNumber;
+
+//! Project version string for AnyUserDefaults.
+FOUNDATION_EXPORT const unsigned char AnyUserDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnyUserDefaults/PublicHeader.h>
+
+
